571f6c75425cb97ac7bc6846655c08c19408d97f
Shawn Goff <shawngoff@Shawns-MacBook-Pro.local>
2023-06-01 10:57:27 -0700

    adds category index action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,18 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    render :index
+  end
+
+  def show
+    @category = Category.find_by(id: params["id"])
+    render :show
+  end
+
+  def create
+    @category = Category.create(
+      name: params[:category][:name],
+    )
+    redirect_to "/categories"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :categories
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  get "/categories" => "categories#index"
+  get "/categories/:id" => "categories#show"
+  post "/categories" => "categories#create"
 end
