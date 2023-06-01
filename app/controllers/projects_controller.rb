4d28761839ae46e9eaa8c5737943622931797513
Eli <eghinshaw@gmail.com>
2023-06-01 13:07:39 -0500

    added edit update actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,4 +25,22 @@ class ProjectsController < ApplicationController
     )
     redirect_to "/projects"
   end
+
+  def edit
+    @project = Project.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @project = Project.find_by(id: params[:id])
+    @project.update(
+      category_id: params[:project][:category_id],
+      title: params[:project][:title],
+      description: params[:project][:description],
+      goal_amount: params[:project][:goal_amount],
+      start_date: params[:project][:start_date],
+      end_date: params[:project][:end_date],
+    )
+    redirect_to "/projects"
+  end
 end
