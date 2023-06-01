ae7657c0fab89f3da865d0fe63db3805efd0e3fa
Jonathan Mendler <Jon.mendler@gmail.com>
2023-06-01 12:56:01 -0500

    add new action & view for Pledge

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -8,4 +8,17 @@ class PledgesController < ApplicationController
     @pledge = Pledge.find_by(id: params[:id])
     render :show
   end
+
+  def new
+    @pledge = Pledge.new
+    render :new
+  end
+
+  def create
+    @pledge = Pledge.create(
+      user_id: params[:pledge][:user_id],
+      reward_id: params[:pledge][:reward_id],
+    )
+    redirect_to "/pledges"
+  end
 end
