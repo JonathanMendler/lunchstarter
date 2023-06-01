5dd6f7c66e3524fc4457e5b66cd05d567367461c
Jonathan Mendler <Jon.mendler@gmail.com>
2023-06-01 12:41:01 -0500

    add post action & view for Pledge

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -3,4 +3,9 @@ class PledgesController < ApplicationController
     @pledges = Pledge.all
     render :index
   end
+
+  def show
+    @pledge = Pledge.find_by(id: params[:id])
+    render :show
+  end
 end
