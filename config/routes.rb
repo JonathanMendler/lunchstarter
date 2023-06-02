cb6a784a9679acdd59041a79a0245ea55fdedec4
Eli <eghinshaw@gmail.com>
2023-06-02 12:41:27 -0500

    created user login, signup abilities, and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   resources :rewards
 
   resources :categories
+
   resources :pledges
+
   resources :projects
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "create" do
+    assert_difference "User.count", 1 do
+      post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
+      assert_response 201
+    end
+  end
+end
